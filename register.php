055970c2f1008dfc4cc02c7a035b84346485481a
aanya97 <itsmeaanyaverma@gmail.com>
2026-03-26 08:24:35 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,35 @@
 
+<?php
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // secure hashing
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name','$email','$password')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Signup successful! <a href='login.php'>Login here</a>";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h2>Register</h2>
+    <form method="POST">
+        <input type="text" name="name" placeholder="Name" required><br><br>
+        <input type="email" name="email" placeholder="Email" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <button type="submit">Register</button>
+    </form>
+    <p>Already have an account? <a href="login.php">Login here</a></p>
+</body>
+</html>
