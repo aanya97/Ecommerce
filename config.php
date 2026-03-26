e4f0539f6fd42ce314c0a07f9ca9b50d7ad73f26
aanya97 <itsmeaanyaverma@gmail.com>
2026-03-26 08:21:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,13 @@
 
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "ecommerce";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
